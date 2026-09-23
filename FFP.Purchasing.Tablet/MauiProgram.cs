010bb994fbe17c4a131d70c5f5ef2bd08e5534cc
Fitzroy Wright <fwright@thinkingmachines.ltd>
2026-09-22 22:32:59 -0500

    Import purchasing application: FFP.Purchasing.Tablet/MauiProgram.cs

---
+++ FFP.Purchasing.Tablet/MauiProgram.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.Logging;
+using FFP.Purchasing.Tablet.Services;
+
+namespace FFP.Purchasing.Tablet;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>();
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+        var apiBase = Preferences.Default.Get("PurchasingApiBaseUrl", "https://purchasing.ffpja.org/");
+        builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(apiBase) });
+        builder.Services.AddSingleton<IAttachmentService, AttachmentService>();
+        builder.Services.AddSingleton<IPurchaseStore, PurchaseStore>();
+        builder.Services.AddSingleton<IReferenceDataService, ReferenceDataService>();
+        builder.Services.AddSingleton<ISyncService, SyncService>();
+        builder.Services.AddSingleton<IAppDiagnostics, AppDiagnostics>();
+        builder.Services.AddSingleton<MainPage>();
+        return builder.Build();
+    }
+}
